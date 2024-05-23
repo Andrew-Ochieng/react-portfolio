c222ff9b5fed1684673705b1bd6a7f4437e1b945
root <andrewomosh44@gmail.com>
2024-05-23 18:58:09 +0300

    add pnpm && skill-icon-styles

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -3,29 +3,32 @@ const Skills = () => {
         <section id="skills">
             <h1 className="section-title">Skills</h1>
             <div className="grid md:grid-cols-8 grid-cols-4 gap-8">
-                <img width="42" height="42" alt="Ruby" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-plain.svg"/>
-                <img width="42" height="42" alt="Rails" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain.svg"/>
-                <img width="42" height="42" alt="Nodejs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg"/>
-                <img width="42" height="42" alt="Express" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"/>
+                <img className='skills-img' src="https://icongr.am/devicon/javascript-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/python-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/ruby-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/django-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/nodejs-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/rails-original-wordmark.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/typescript-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/react-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/vuejs-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/bootstrap-plain.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/html5-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/css3-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/postgresql-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/mongodb-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/mysql-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' width="60" height="60" alt="Firebase" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"/>
+                <img className='skills-img' width="60" height="60" alt="Firebase" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/supabase/supabase-plain.svg"/>
+                <img className='skills-img' src="https://icongr.am/devicon/git-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/github-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' src="https://icongr.am/devicon/docker-original.svg?size=60&color=currentColor" alt="" />
+                
+                <img className='skills-img' src="https://icongr.am/devicon/wordpress-original.svg?size=60&color=currentColor" alt="" />
+                <img className='skills-img' width="60" height="60" alt="Figma" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" />
+                <img className='skills-img' width="60" height="60" alt="Illustrator" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/illustrator/illustrator-plain.svg"/>
+                <img className='skills-img' width="60" height="60" alt="Photoshop" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-plain.svg"/>
 
-                <img width="42" height="42" alt="TypeScript" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg"/>
-                <img width="42" height="42" alt="React" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
-                <img width="42" height="42" alt="Vue" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg" />
-                <img width="42" height="42" alt="Bootstrap" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg"/>
-                <img  width="42" height="42" alt="Tailwindcss" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" />
-                <img width="42" height="42" alt="CSS" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg"/>
-                <img width="42" height="42" alt="HTML" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg"/>
-                <img width="42" height="42" alt="JavaScript" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"/>
-                <img width="42" height="42" alt="PostGres" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain.svg"/>
-                <img width="42" height="42" alt="Mysql" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg"/>
-                <img width="42" height="42" alt="MongoDB" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg"/>
-                <img width="42" height="42" alt="Firebase" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"/>
-                <img width="42" height="42" alt="Wordpress" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-plain.svg"/>
-
-                <img width="42" height="42" alt="Figma" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" />
-                <img width="42" height="42" alt="Illustrator" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/illustrator/illustrator-plain.svg"/>
-                <img width="42" height="42" alt="Photoshop" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-plain.svg"/>
-        
             </div>
             <hr className="section-border"/> 
         </section>
