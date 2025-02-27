78e2ffd575a6fcfd87aa3f781963226167b3be31
Andrew-Ochieng <andrewomosh44@gmail.com>
2025-02-27 16:08:19 +0300

    fix skills cards: mobile-view

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -9,8 +9,8 @@ const Skills = () => {
             <h1 className="section-title">Professional Skills</h1>
             <div className="grid md:grid-cols-6 grid-cols-3 md:gap-8 gap-4 items-center">
                 {skillsData.map((skill, index) => (
-                    <figure className="flex flex-col items-center justify-center bg-slate-800 shadow-md shadow-gray-800 hover:-translate-y-2 border-2 border-slate-800  hover:border-cyan-500 duration-300 md:p-4 p-2 md:w-32 w-28 rounded-lg">
-                        <img key={index} className='md:w-12 w-10' src={skill.src} alt={skill.name} loading="lazy" decoding="async" />
+                    <figure className="flex flex-col items-center justify-center bg-slate-800 shadow-md shadow-gray-800 hover:-translate-y-2 border-2 border-slate-800  hover:border-cyan-500 duration-300 md:p-4 p-2 md:w-32 w-26 rounded-lg">
+                        <img key={index} className='md:w-12 w-9' src={skill.src} alt={skill.name} loading="lazy" decoding="async" />
                         <span className="mt-2">{skill.name}</span>
                     </figure>
                 ))}
